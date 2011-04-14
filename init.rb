a4773232696c0963ae3e62c90eb905017617d75a
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-04-14 16:52:28 +0400

    Extend requirements

--- init.rb
+++ init.rb
@@ -7,7 +7,8 @@ Redmine::Plugin.register :redmine_close_button do
   author 'akzhan.abdulin@gmail.com'
   description 'This is the Close Issue button plugin for Redmine'
   version '0.0.4'
-  url 'http://github.com/Undev/redmine_close_button'
+  url 'http://github.com/Undev/redmine_close_button' if respond_to?(:url)
   author_url 'http://github.com/Undev'
+  requires_redmine :version_or_higher => '0.9.0'
 end
 
