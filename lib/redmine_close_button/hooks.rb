67a2e3f5b60dcd1135953a1cc5947cce51e072a4
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-05-25 12:16:16 +0400

    I18n already included by base Redmine::Hook::Listener class from 2009-02-21.

--- lib/redmine_close_button/hooks.rb
+++ lib/redmine_close_button/hooks.rb
@@ -1,7 +1,5 @@
 module RedmineCloseButton
   class Hooks < Redmine::Hook::ViewListener
-    include Redmine::I18n
-
     render_on :view_issues_form_details_bottom, :partial => 'issues/close_button'
   end
 end
