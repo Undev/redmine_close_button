0c6878c87c1797b0b21579034a578c303d1ff4cd
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2012-03-20 16:47:18 +0400

    Use another hook to be compatible with Redmine 1.3.x, fixes GH#18, thanks to @avkvl.

--- lib/redmine_close_button/hooks.rb
+++ lib/redmine_close_button/hooks.rb
@@ -1,5 +1,5 @@
 module RedmineCloseButton
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_form_details_bottom, :partial => 'issues/close_button'
+    render_on :view_issues_show_details_bottom, :partial => 'issues/close_button'
   end
 end
