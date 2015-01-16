8442a21e86fdf8533b3d434b47d3b64c3b564d22
Irina Lebedevich <irina.lebedevich@undev.ru>
2015-01-16 12:31:37 +0300

    ref #56810 Plugin description slightly reworded.

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require_dependency 'redmine_close_button/hooks'
 Redmine::Plugin.register :redmine_close_button do
   name 'Redmine Close Issue Button Plugin'
   author 'Undev'
-  description 'The Close Issue button that enables you to close issues quickly.'
+  description 'This plugins enables you to close issues quickly using the Close Issue button.'
   version '0.0.8'
   url 'http://github.com/Undev/redmine_close_button' if respond_to?(:url)
   author_url 'http://github.com/Undev'
